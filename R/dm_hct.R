cda468e8948968d5ae1c00e3bc4d36629b3637f0
Jesse Smith <jesse.smith@stjude.org>
2023-04-18 10:17:48 -0500

    Add pivoting to `dm_hct()`

--- R/dm_hct.R
+++ R/dm_hct.R
@@ -17,22 +17,20 @@ dm_hct <- function(
   as_rlang_error(checkmate::assert_flag(.quiet))
   style_bold <- if (rlang::is_installed("cli")) cli::style_bold else function(x) x
   # Extract data from database
-  if (!.quiet) {
-    rlang::inform(style_bold("Extracting database (1/3)"))
-  }
-  dm <- dm_extract(
-    dm_remote = dm_remote,
-    ...,
-    .legacy = FALSE,
-    .excl_dsmb = .excl_dsmb
-  )
+  if (!.quiet) rlang::inform(style_bold("Extracting database (1/4)"))
+  dm <- dm_extract(dm_remote = dm_remote, ..., .legacy = FALSE, .excl_dsmb = .excl_dsmb)
   if (!.quiet) rlang::inform("Done.")
   # Standardize columns
-  if (!.quiet) rlang::inform(style_bold("Standardizing table columns (2/3)"))
+  if (!.quiet) rlang::inform(style_bold("Standardizing table columns (2/4)"))
   dm <- dm_standardize(dm_local = dm, quiet = .quiet)
   # Combine tables
-  if (!.quiet) rlang::inform(style_bold("Combining similar tables (3/3)"))
+  if (!.quiet) rlang::inform(style_bold("Combining similar tables (3/4)"))
   dm <- dm_combine(dm)
   if (!.quiet) rlang::inform("Done.")
+  # Pivot tables
+  if (!.quiet) rlang::inform(style_bold("Pivoting E-A-V tables (4/4)"))
+  dm <- dm_pivot(dm)
+  if (!.quiet) rlang::inform("Done.")
+  # Return
   dm
 }
